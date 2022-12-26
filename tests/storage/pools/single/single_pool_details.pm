f4f237635842c0ff32b0939d712a7ec70260bf0a
FroggyFlox <flox2b@gmail.com>
2022-12-26 11:02:57 -0500

    Start adding single pool tests
    Simply copy existing files from raid1 tests that will later be adjusted
    for testing single pools.

---
+++ tests/storage/pools/single/single_pool_details.pm
@@ -0,0 +1,50 @@
+# Copyright (c) 2012-2022 RockStor, Inc. <http://rockstor.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+# This module is used to navigate to the Storage > Pools page
+
+
+use base 'basetest';
+use warnings;
+use strict;
+use testapi;
+use lockapi;
+
+sub run {
+
+    # Launch mutex to signal supportserver the system is ready
+    # to populate the pool with data
+    mutex_create 'raid1_pool_created';
+
+    # Wait on mutex ready for population of data
+    mutex_wait 'raid1_pool_populated';
+
+    # Click on Pool name
+    assert_and_click('pools_page_raid1-pool_created_click_name', 'timeout' => 120);
+
+    # Verify page layout and raid configuration
+    assert_screen('raid1-pool_details_page', 'timeout' => 120);
+}
+
+sub test_flags {
+    # without anything - rollback to 'lastgood' snapshot if failed
+    # 'fatal' - whole test suite is in danger if this fails
+    # 'milestone' - after this test succeeds, update 'lastgood'
+    # 'important' - if this fails, set the overall state to 'fail'
+    return { fatal => 1 };
+}
+
+1;
---
+++ tests/storage/pools/single/supportserver_single_populate_pool.pm
@@ -0,0 +1,59 @@
+# Copyright (C) 2020-2021 SUSE LLC
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+# Here, we simply test if we have an IP address. If we do, we let the
+# children know we are ready.
+
+
+use base 'basetest';
+use warnings;
+use strict;
+use testapi;
+use lockapi;
+use mmapi;
+
+sub run {
+    # Clear the screen
+    enter_cmd('clear');
+
+    # Wait for mutex signal to populate pool
+    ## Follow example in docs: https://open.qa/docs/#_test_synchronization_and_locking_api
+    ## When a parent (this job) needs to wait for a child, let's fetch the child ID first
+    ## and pass this to mutex_wait
+    my $children = get_children();
+    # We only have one child here
+    my $child_id = (keys %$children)[0];
+    # Pass this child_id to mutex_wait
+    mutex_wait('raid1_pool_created', $child_id);
+
+    # Populate pool
+    ## copy / to /mnt2/raid1_pool
+    assert_script_run('ls -lah /mnt2/raid1-pool/');
+    assert_script_run('cp -R /{usr,var} /mnt2/raid1-pool/.');
+
+    # Send mutex ready signal
+    mutex_create 'raid1_pool_populated';
+}
+
+sub test_flags {
+    # without anything - rollback to 'lastgood' snapshot if failed
+    # 'fatal' - whole test suite is in danger if this fails
+    # 'milestone' - after this test succeeds, update 'lastgood'
+    # 'important' - if this fails, set the overall state to 'fail'
+    return { fatal => 1 };
+}
+
+1;
