f8969bc0335a90e3127c5e32ae2476d64d6e8cc1
FroggyFlox <flox2b@gmail.com>
2022-12-27 11:10:49 -0500

    Minor cleanup and comments

--- establish_config.py
+++ establish_config.py
@@ -27,9 +27,6 @@ TEST_MACHINE_SETTINGS = [
 
 client = OpenQA_Client(server="openqa-server", scheme="http")
 current_machines = client.openqa_request("GET", "machines")["Machines"]
-# client.openqa_request(
-#     "POST", "machines", params=TEST_MACHINE_SETTINGS
-# )
 
 for machine in TEST_MACHINE_SETTINGS:
     # Search whether a machine with the same name already exists
@@ -38,7 +35,9 @@ for machine in TEST_MACHINE_SETTINGS:
         for matching_dict in current_machines
         if matching_dict["name"] == machine["name"]
     ]
+    # Delete any existing matching with the same name
     if len(matching_machine) > 0:
         matching_machine_id = matching_machine[0]["id"]
         client.openqa_request("DELETE", f"machines/{matching_machine_id}")
+    # Define our machine
     client.openqa_request("POST", "machines", params=machine)
